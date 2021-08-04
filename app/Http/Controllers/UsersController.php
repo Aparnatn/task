5ba8b416fa838c163460f7e2cfa9fa55fefa629b
Aparnatn <aparnaneelakandhan135@gmail.com>
2021-08-04 22:57:20 +0530

    first commit

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Users;
+class UsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = Users::all();
+
+        return view('users.index',compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+        ]);
+
+        Users::create($request->all());
+
+        return redirect()->route('users.index')->with('success','user created successfully.');
+     }
+
+
+    /**
+     * Display the specified resource.
+     *
+      *@param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return view('users.show',compact('users'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Users $users)
+    {
+        return view('users.edit',compact('users'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Users $users)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+        ]);
+
+        $users->update($request->all());
+
+        return redirect()->route('users.index')->with('success','user updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Users $users)
+    {
+        $users->delete();
+
+         return redirect()->route('users.index')
+                         ->with('success','user deleted successfully');
+    }
+
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,42 @@
+@extends('layout.app')
+@section('content')
+
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-success" href="{{ route('users.create') }}"> Create New Product</a>
+            </div>
+        </div>
+    </div>
+    @if ($message = Session::get('success'))
+    <div class="alert alert-success">
+        <p>{{ $message }}</p>
+    </div>
+@endif
+
+<table class="table table-bordered">
+    <tr>
+        <th>Name</th>
+        <th>Email</th>
+    </tr>
+    @foreach ($users as $users)
+    <tr>
+        <td>{{ $users->Route::post('user', 'UserController@index')->name('user'); }}</td>
+        <td>{{ $users->email }}</td>
+        <td>
+            <form action="{{ route('users.destroy',$users->id) }}" method="POST">
+                <a class="btn btn-info" href="{{ route('users.show',$users->id) }}">Show</a>
+                <a class="btn btn-primary" href="{{ route('users.edit',$users->id) }}">Edit</a>
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-danger">Delete</button>
+            </form>
+        </td>
+    </tr>
+    @endforeach
+</table>
+
+@endsection
